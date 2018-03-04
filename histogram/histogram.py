c893ff5253ed58c80398ca616d8ef8d7374df1fa
nekonee <haniasa96@gmail.com>
2018-03-04 22:36:15 +0100

    Minor details

--- histogram/histogram.py
+++ histogram/histogram.py
@@ -14,14 +14,16 @@ print(image.shape, image.dtype)
 
 
 def histogram(image):
-    wartosci = [0 for _ in range(256)]
+    hist = [0 for _ in range(256)]
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-             w = image[i,j]
-             wartosci[w] += 1
+             bins = image[i,j]
+             hist[bins] += 1
              
     for w in range(256):
         print("Value: ", w, "\n number of pixels: ", wartosci[w])
+        
+        return hist, bins
 
 
 histogram(image)
